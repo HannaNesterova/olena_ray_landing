c9aa2fc28833b47b7dcd5be1962e9688247053cc
Hanna Nesterova <nesterova.hanna@gmail.com>
2024-02-26 12:44:09 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import styles from './App.module.css';
+import NavBar from './components/NavBar/NavBar';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <NavBar />
+      <h1>TEST</h1>
+    </div>
+  );
+}
+
+export default App;
